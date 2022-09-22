ff13b70860f1bec0215d371d401599d282ff8ee1
James Ogilvie <jamesogilvie@Jamess-MacBook-Air.local>
2022-09-22 14:24:17 +1000

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by James Ogilvie on 22/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
